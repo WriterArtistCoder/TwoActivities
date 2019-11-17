fa5e44b7f347e56395a8d1f2254fb57588f66282
WriterArtistCoder <percyghenburg@gmail.com>
2019-11-17 11:31:59 -0800

    Finished 4.1 - 4.3 of Task 4, will start 4.4 next time

--- app/src/main/java/com/tinystripz/twoactivities/SecondActivity.java
+++ app/src/main/java/com/tinystripz/twoactivities/SecondActivity.java
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY =
+            "com.tinystripz.twoactivities.extra.REPLY";
+    private EditText mReply;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +24,16 @@ public class SecondActivity extends AppCompatActivity {
 
         TextView textView = findViewById(R.id.text_message);
         textView.setText(message);
+    }
+
+    public void returnReply(View view) {
+        mReply = findViewById(R.id.editText_second);
+
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, replyIntent);
+
+        setResult(RESULT_OK, replyIntent);
+        finish();
     }
 }
